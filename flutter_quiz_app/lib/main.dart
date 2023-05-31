2212210d25edcb9bea61b1e17faef95591ca0da9
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-05-31 22:47:04 +0100

    switch screen

--- flutter_quiz_app/lib/main.dart
+++ flutter_quiz_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz_app/start_screen.dart';
+import 'package:flutter_quiz_app/quiz.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,23 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 23, 85, 199),
-                Color.fromARGB(255, 73, 143, 248),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
+    return Quiz();
   }
 }
---
+++ flutter_quiz_app/lib/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/questions_screen.dart';
+import 'package:flutter_quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = "start-screen";
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = "questions-screen";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 23, 85, 199),
+                Color.fromARGB(255, 73, 143, 248),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen == "start-screen"
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
+        ),
+      ),
+    );
+  }
+}
