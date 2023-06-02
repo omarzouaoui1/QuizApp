286d35a48b376801aaba71335832d54a1f05dbfd
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-02 18:29:59 +0100

    Alignment, Margin & Padding buttons

--- flutter_quiz_app/lib/questions_screen.dart
+++ flutter_quiz_app/lib/questions_screen.dart
@@ -15,25 +15,29 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          ...currentQuestion.answers.map(
-            (answer) {
-              return AnswerButton(
-                answerText: answer,
-                onTap: () {},
-              );
-            },
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            ...currentQuestion.answers.map(
+              (answer) {
+                return AnswerButton(
+                  answerText: answer,
+                  onTap: () {},
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
