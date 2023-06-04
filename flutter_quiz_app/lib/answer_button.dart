6e49302040b918e720b1e0c934ee47e32e0ae869
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-04 15:13:26 +0100

    moving on questions

--- flutter_quiz_app/lib/answer_button.dart
+++ flutter_quiz_app/lib/answer_button.dart
@@ -13,7 +13,7 @@ class AnswerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onTap,
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
         backgroundColor: const Color.fromARGB(232, 21, 0, 80),
--- flutter_quiz_app/lib/questions_screen.dart
+++ flutter_quiz_app/lib/questions_screen.dart
@@ -10,9 +10,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
       child: Padding(
@@ -35,7 +43,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               (answer) {
                 return AnswerButton(
                   answerText: answer,
-                  onTap: () {},
+                  onTap: answerQuestion,
                 );
               },
             )
